403933b18770c6b96b5b46778f2cb14bc66ae7b5
siddhi21-code <siddhishirke508@gmail.com>
2025-07-11 22:05:15 +0530

    ok

---
+++ Day7/script.js
@@ -0,0 +1,36 @@
+//loops in js 
+//for loop
+//ex1
+for (let i = 1 ; i<=8 ; i++){
+    console.log("Easy Code");
+}
+
+//(while loop ex)
+//ex 1
+let number = 0;
+while( number <= 100){
+    if (number % 2 == 0){ 
+        console.log(number)
+    }
+ number ++
+}
+
+ //(do while loop)
+ //ex1
+ let num = 0;
+ do {
+    console.log(num);
+    num ++
+ } while (num < 7);
+ 
+ //for-of loop
+ //ex1
+ let str ="Easy Code";
+ for( let i of str){
+    console.log("i=", i);
+ }
+
+ 
+
+
+
---
+++ Day8/script.js
@@ -0,0 +1,24 @@
+
+//loops
+//ex1 do while 
+let num = 0;
+ do {
+    console.log(num);
+    num ++
+ } while (num < 7);
+    
+ //ex1 for loop
+ let i = 0;
+ for(i; i< 10; i ++){
+    console.log(i)
+ }
+
+//ex 3 sum nos for loop ex 
+
+let sum = 0;
+for(let i = 1; i<= 25; i++){
+   sum += i;
+}
+console.log("The sum of first 25 number is :"+ sum);
+
+
